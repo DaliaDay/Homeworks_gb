023bb47b56e3687ae51501d62bab5028aa56b440
Dildora <1910076.dildora.asadova@gmail.com>
2023-03-14 14:03:28 +0500

    Chaged task4 and task5 is not yet complited

--- HW_C1/task4/Program.cs
+++ HW_C1/task4/Program.cs
@@ -3,11 +3,11 @@
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
 
-Console.WriteLine("Hello! Please Enter a numbers to represent all the even number form 0 to number N");
+Console.WriteLine("Hello! Please Enter a number to represent all the even number form 0 to number N");
 int num = Convert.ToInt32(Console.ReadLine());
-int even_num = 0;
-for(num = 1; num > 0; num++){
-    even_num = even_num + num/2;
-
+int even_num = 1;
+while(even_num < num){
+    Console.WriteLine($"-> {even_num+1}" );
+    even_num += 2;
 }
-Console.WriteLine($"The collection of even numbers are:  {even_num}");
+// Console.WriteLine($"The collection of even numbers are:  {even_num}");
---
+++ HW_C1/task5/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 7 HARD по желанию - идет за 2 необязательных Напишите программу, которая принимает на вход целое число любой разрядности и на выходе показывает вторую цифру слева этого числа или говорит, что такой цифры нет. Через строку решать нельзя.
+// 456111 -> 5
+// 78 -> 8
+// 9146548 -> 1
+// 3 -> нет
+
+Console.WriteLine("Hello! Please Enter a number to read a second digit from the start of the number");
+int num = Convert.ToInt32(Console.ReadLine());
+List<int> lst = new List<Int32>(num);
+
+if(num >= 10){
+    
+    Console.WriteLine(lst[1]);
+}
+else{
+    Console.WriteLine("Sorry but there is no second digit number in the entered number. Please enter a number > 10 ");
+
+}
