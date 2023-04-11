462b319c6bff2a8431344f9c3c8100de1adc3116
Dildora <1910076.dildora.asadova@gmail.com>
2023-04-11 16:04:49 +0500

    Task3 C#

---
+++ HW_C5/task3/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+int[] array = new int[6];
+void FillArray(int[] array)
+{
+    for (int i=0; i<array.Length;i++)
+        array[i]=new Random().Next(1,100);
+}
+
+void PrintArray(int[] array)
+{
+    foreach (int item in array)
+        Console.Write($"{item} ");
+Console.WriteLine();
+}
+
+void DifferenceMinMax(int[] array)
+{ 
+    int max = array[0];
+    int min  = array[0];
+for(int index = 0; index < array.Length; index++)
+{ 
+    if (array[index]>max)
+    {
+        max = array[index];
+    }
+    if (array[index]<min)
+    {
+        min = array[index];
+    }  
+}
+int difference  = max - min;
+ Console.WriteLine($"Maximum Element -> {max}");
+ Console.WriteLine($"Minimum Element -> {min}");
+ Console.WriteLine($"Difference between Max and Min is -> {difference}");
+}
+
+
+
+
+FillArray(array);
+PrintArray(array);
+DifferenceMinMax(array);
